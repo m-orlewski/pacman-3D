6b339d2f1bfcd98e7e8a5f0022018d8945cd0fce
morlewski <morlewski@student.agh.edu.pl>
2022-11-14 01:37:38 +0100

    add game states

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import pyray as pr
+from enum import Enum
 
 from map import Map
+
+class GameState(Enum):
+    GAMEPLAY = 0
+    PAUSED = 1
+    GAME_OVER = 2
+    GAME_WON = 3
+    
 
 if __name__ == '__main__':
     pr.init_window(1920, 1080, "Pacman 3D")
@@ -11,17 +19,26 @@ if __name__ == '__main__':
 
     map = Map()
 
+    gameState = GameState.GAMEPLAY
+
     while not pr.window_should_close():
         pr.update_camera(camera)
 
         map.update()
+
         
         pr.begin_drawing()
         pr.clear_background(pr.RAYWHITE)
         pr.begin_mode_3d(camera)
 
-        
-        map.draw()
+        if gameState == GameState.GAMEPLAY:
+            map.draw()
+        elif gameState == GameState.PAUSED:
+            pass #TODO
+        elif gameState == GameState.GAME_WON:
+            pass #TODO
+        elif gameState == GameState.GAME_OVER:
+            pass #TODO
 
         pr.end_mode_3d()
         pr.end_drawing()
