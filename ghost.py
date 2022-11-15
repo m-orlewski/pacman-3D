34a95046e7d065c270dbd2fe04f110a938d17f56
morlewski <morlewski@student.agh.edu.pl>
2022-11-15 22:07:56 +0100

    ghost movement working

--- ghost.py
+++ ghost.py
@@ -13,6 +13,7 @@ class Ghost():
         self.color = color
 
         self.currentDirection = currentDirection
+        self.oppositeDirection = (self.currentDirection + 2) % 4
         self.stepCount = 0
 
     def draw(self):
@@ -20,24 +21,37 @@ class Ghost():
 
     def check_collisions(self, walls, newPos):
         for wall in walls:
-            boudingBox = pr.BoundingBox(pr.Vector3(wall.pos.x - CELL_SIZE/2, wall.pos.y - CELL_SIZE/2, wall.pos.z - CELL_SIZE/2),
+            wallBoudingBox = pr.BoundingBox(pr.Vector3(wall.pos.x - CELL_SIZE/2, wall.pos.y - CELL_SIZE/2, wall.pos.z - CELL_SIZE/2),
                                         pr.Vector3(wall.pos.x + CELL_SIZE/2, wall.pos.y + CELL_SIZE/2, wall.pos.z + CELL_SIZE/2))
-            if pr.check_collision_box_sphere(boudingBox, newPos, GHOST_RADIUS):
-                return True
-        return False
+            ghostBoundingBox = pr.BoundingBox(pr.Vector3(newPos.x - CELL_SIZE/2 + 0.0001, newPos.y - CELL_SIZE/2 + 0.0001, newPos.z - CELL_SIZE/2 + 0.0001),
+                                        pr.Vector3(newPos.x + CELL_SIZE/2 - 0.0001, newPos.y + CELL_SIZE/2 - 0.0001, newPos.z + CELL_SIZE/2 - 0.0001))
+            if pr.check_collision_boxes(wallBoudingBox, ghostBoundingBox):
+                return False
+        return True
 
     def updateDirection(self, walls):        
         newPosUp = pr.Vector3(self.pos.x, self.pos.y, self.pos.z - GHOST_SPEED)
         newPosRight = pr.Vector3(self.pos.x + GHOST_SPEED, self.pos.y, self.pos.z)
         newPosDown = pr.Vector3(self.pos.x, self.pos.y, self.pos.z + GHOST_SPEED)
-        newPosUp = pr.Vector3(self.pos.x - GHOST_SPEED, self.pos.y, self.pos.z)
+        newPosLeft = pr.Vector3(self.pos.x - GHOST_SPEED, self.pos.y, self.pos.z)
 
-        
-            
+        canMove = [self.check_collisions(walls, newPosUp), self.check_collisions(walls, newPosRight), self.check_collisions(walls, newPosDown),self.check_collisions(walls, newPosLeft)]
+        print(canMove)
+        if canMove.count(True) == 2 and canMove[self.currentDirection] and canMove[self.oppositeDirection]:
+            return # can only go the same direction or turn back, so go ahead
+        elif canMove.count(True) == 1:
+            self.currentDirection, self.oppositeDirection = self.oppositeDirection, self.currentDirection # in case of dead end, turn back
+        else:
+            canMove[self.oppositeDirection] = False
+            availableDirections = [i for i, value in enumerate(canMove) if value == True]
+            print(availableDirections)
+            self.currentDirection = random.choice(availableDirections)
+            self.oppositeDirection = (self.currentDirection + 2) % 4
 
     def move(self, walls):
         if self.stepCount == 20:
             self.updateDirection(walls)
+            self.stepCount = 0
 
         if self.currentDirection == 0:
             self.pos.z -= GHOST_SPEED
